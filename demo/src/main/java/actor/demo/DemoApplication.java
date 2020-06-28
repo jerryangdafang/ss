ac2cd510592e349c9395579deb697141998f2897
Administrator <1924957577@qq.com>
2020-06-28 21:17:52 +0800

    23324567rerthyjuki,

---
+++ demo/src/main/java/actor/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package actor.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableAdminServer   asdgfhjmkvdfbgnh
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
